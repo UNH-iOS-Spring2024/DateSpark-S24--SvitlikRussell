fe2b965ded129c0e0b1b1f3277213e7261b761b3
Shannon <s.russell3271@gmail.com>
2024-03-19 19:51:43 -0400

    Copying Files From Old Repo

--- DateSpark/DateSparkApp.swift
+++ DateSpark/DateSparkApp.swift
@@ -1,11 +1,22 @@
 
 import SwiftUI
+import Firebase
 
 @main
 struct DateSparkApp: App {
+    init(){
+        let providerFactory = AppCheckDebugProviderFactory()
+        AppCheck.setAppCheckProviderFactory(providerFactory)
+        
+        FirebaseApp.configure()
+        }
+    @StateObject private var archiveViewModel = ArchiveViewModel() // Initialize the shared data model
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(archiveViewModel)
         }
     }
 }
+
