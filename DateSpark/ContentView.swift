dce1b1677b6d179e39ffb01b71c937fa2b3d782b
Shannon <s.russell3271@gmail.com>
2024-03-19 19:07:13 -0400

    Initial Commit

---
+++ DateSpark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DateSpark
+//
+//  Created by Shannon Russell on 3/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "person.circle")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Initial Commit Testing")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DateSpark/DateSparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateSparkApp.swift
+//  DateSpark
+//
+//  Created by Shannon Russell on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct DateSparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
