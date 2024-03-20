d5e1d72ed432a9bf400c5998dcf6a9d15d17f0a9
sarahsvitlik03 <sarahtheawesome03@gmail.com>
2024-03-19 20:31:56 -0400

    triangle added to view
    for some reason this makes the wheel super small

--- DateSpark/HomePage/Model/DateClass.swift
+++ DateSpark/HomePage/Model/DateClass.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+class DateClass: ObservableObject{
+    
+    var id: String
+    
+    @Published var title: String
+    @Published var description: String
+    @Published var outfit: String
+    @Published var time: String
+    @Published var weather: String
+    @Published var portion: Double
+    @Published var rating: String
+
+    required init?(id: String, data: [String: Any]) {
+              let title = data["title"] as? String != nil ? data["title"] as! String : ""
+              let description = data["description"] as? String != nil ? data["description"] as! String : ""
+              let outfit = data["outfit"] as? String != nil ? data["outfit"] as! String : ""
+              let time = data["time"] as? String != nil ? data["time"] as! String : ""
+              let weather = data["weather"] as? String != nil ? data["weather"] as! String : ""
+              let portion = data["portion"] as? Double != nil ? data["portion"] as! Double : 50
+              let rating = data["rating"] as? String != nil ? data["rating"] as! String : ""
+
+
+        self.id = id
+        self.title = title
+        self.description = description
+        self.outfit = outfit
+        self.time = time
+        self.weather = weather
+        self.portion = portion
+        self.rating = rating
+    }
+}
--- DateSpark/HomePage/View/HomeView.swift
+++ DateSpark/HomePage/View/HomeView.swift
@@ -9,39 +9,6 @@ import SwiftUI
 import Charts
 import FirebaseFirestore
 
-class DateClass: ObservableObject{
-    
-    var id: String
-    
-    @Published var title: String
-    @Published var description: String
-    @Published var outfit: String
-    @Published var time: String
-    @Published var weather: String
-    @Published var portion: Double
-    @Published var rating: String
-
-    required init?(id: String, data: [String: Any]) {
-              let title = data["title"] as? String != nil ? data["title"] as! String : ""
-              let description = data["description"] as? String != nil ? data["description"] as! String : ""
-              let outfit = data["outfit"] as? String != nil ? data["outfit"] as! String : ""
-              let time = data["time"] as? String != nil ? data["time"] as! String : ""
-              let weather = data["weather"] as? String != nil ? data["weather"] as! String : ""
-              let portion = data["portion"] as? Double != nil ? data["portion"] as! Double : 50
-              let rating = data["rating"] as? String != nil ? data["rating"] as! String : ""
-
-
-        self.id = id
-        self.title = title
-        self.description = description
-        self.outfit = outfit
-        self.time = time
-        self.weather = weather
-        self.portion = portion
-        self.rating = rating
-    }
-}
-
 struct HomeView: View {
     @State private var isShowingPopover = false
     @State var txtchoice: String = ""
@@ -73,6 +40,15 @@ struct HomeView: View {
                 .padding(.top, 100)
                 
                 PieChartView(dataPoints: $dates)
+                
+                VStack{
+                    Image(systemName: "triangle")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    
+                    Spacer()
+                }
                 
                 HStack {
                     Spacer()
