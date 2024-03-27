70bbe2a5a39ab0ace0ee4d921df69e8e9c23d568
Shannon <s.russell3271@gmail.com>
2024-03-26 20:07:51 -0400

    Splash Loading Screen

--- DateSpark/ContentView.swift
+++ DateSpark/ContentView.swift
@@ -5,20 +5,31 @@ class AppVariables: ObservableObject {
     @Published var selectedTab: Int = 0
 }
 struct ContentView: View {
+    @State private var isActive = false
     
     var body: some View {
-        BottomBar (
-            AnyView(HomeView()),
-            AnyView(SparkGPTView()),
-            AnyView(MapView()),
-            AnyView(FriendsView()),
-            AnyView(ProfileView())
-        )
-        .environmentObject(AppVariables())
+        ZStack {
+            if isActive {
+                BottomBar(
+                    AnyView(HomeView()),
+                    AnyView(SparkGPTView()),
+                    AnyView(MapView()),
+                    AnyView(FriendsView()),
+                    AnyView(ProfileView())
+                )
+                .environmentObject(AppVariables())
+                .transition(.opacity)
+            }
+            if !isActive {
+                SplashScreenView(isActive: $isActive)
+            }
+        }
+        .animation(.easeInOut, value: isActive) // Animate the transition
     }
 }
 
-
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
