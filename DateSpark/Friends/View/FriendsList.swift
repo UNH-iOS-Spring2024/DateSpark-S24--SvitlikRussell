bfd6df4c59767c175349893f762e852066590372
Shannon <s.russell3271@gmail.com>
2024-04-15 15:41:50 -0400

    Connecting Friend List view to Add A friend

--- DateSpark/Friends/View/FriendsList.swift
+++ DateSpark/Friends/View/FriendsList.swift
@@ -4,15 +4,20 @@ import SwiftUI
 
 struct FriendsList: View {
     @ObservedObject var viewModel: FriendsViewModel
-    
+    @State private var showingSendFriendRequest = false  
     var body: some View {
         NavigationView {
             List(viewModel.friends, id: \.self) { friend in
                 Text(friend)
             }
             .navigationTitle("Friends List")
-            .onAppear {
-                viewModel.fetchFriends()
+            .navigationBarItems(trailing: Button(action: {
+                showingSendFriendRequest = true
+            }) {
+                Image(systemName: "plus")
+            })
+            .sheet(isPresented: $showingSendFriendRequest) {
+                SendFriendRequestView(viewModel: viewModel)
             }
         }
     }
