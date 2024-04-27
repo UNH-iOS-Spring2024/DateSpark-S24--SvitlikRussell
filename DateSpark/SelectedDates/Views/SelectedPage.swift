286c5673e83af7638d646071de113e4a89d1ca24
sarahsvitlik03 <sarahtheawesome03@gmail.com>
2024-04-27 13:55:22 -0400

    Selected dates page UI updated

--- DateSpark/SelectedDates/Views/SelectedPage.swift
+++ DateSpark/SelectedDates/Views/SelectedPage.swift
@@ -26,57 +26,69 @@ struct SelectedPage: View {
     let db = Firestore.firestore()
     
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            Button(action: {
-                if !isSaved {
-                    saveToFirebase(userId: userId ?? "")
-                }
-                isSaved.toggle() // Toggle saved state
-            }) {
-                Image(systemName: isSaved ? "bookmark.fill" : "bookmark")
-                    .resizable()
-                    .frame(width: 25, height: 25)
-                    .foregroundColor(isSaved ? .pink : .primary)
-            }
-            .padding()
+        
+        VStack{
+            Image("Logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+                .padding(.bottom, -140)
             
+            
+            HStack {
+                Spacer() // Pushes the button to the right
+                Button(action: {
+                    if !isSaved {
+                        saveToFirebase(userId: userId ?? "")
+                    }
+                    isSaved.toggle() // Toggle saved state
+                }) {
+                    Image(systemName: isSaved ? "bookmark.fill" : "bookmark")
+                        .resizable()
+                        .frame(width: 20, height: 25)
+                        .foregroundColor(isSaved ? .pink : .primary)
+                }
+                .padding()
+            }
+                
             VStack{
                 Text(selectedTitle)
                     .font(titleFont)
                     .bold()
-                    .padding(10)
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 20)
                 
                 Text(selectedDescription)
+                    .padding(.horizontal, 20)
                 
-                // Menu for Weather choice
-                Menu(selectedWeather) {
-                    Button("Sunny ☀️") { selectedWeather = "Sunny" }
-                    Button("Rainy 🌧") { selectedWeather = "Rainy" }
-                    Button("Cloudy ☁️") { selectedWeather = "Cloudy" }
-                    Button("Windy 🌬") { selectedWeather = "Windy" }
-                    Button("Snowy ❄️") { selectedWeather = "Snowy" }
-                    Button("Foggy 🌫") { selectedWeather = "Foggy" }
-                    Button("Stormy ⛈") { selectedWeather = "Stormy" }
-                }
-                .padding(10)
-                
-                // Menu for Outfit Choice
-                Menu(selectedOutfit) {
-                    Button("Casual Outfit - Everyday Comfort") { selectedOutfit = "Casual Outfit" }
-                    Button("Formal Outfit - Business and Events") { selectedOutfit = "Formal Outfit" }
-                    Button("Fancy Outfit - Elegant & Luxurious") { selectedOutfit = "Fancy Outfit" }
-                    Button("Vintage Outfit - Timeless Classics") { selectedOutfit = "Vintage Outfit" }
-                    Button("Streetwear Outfit - Urban & Trendy") { selectedOutfit = "Streetwear Outfit" }
-                    Button("Preppy Outfit - Polished & Pre-collegiate") { selectedOutfit = "Preppy Outfit" }
-                    Button("Minimalist Outfit - Simple & Sleek") { selectedOutfit = "Minimalist Outfit" }
-                    Button("Comfy Outfit - Cozy & Relaxed") { selectedOutfit = "Comfy Outfit" }
-                    Button("Artsy Outfit - Creative & Unique") { selectedOutfit = "Artsy Outfit" }
-                }
-                
-                // Ideal Time
                 VStack {
+                    // Menu for Weather choice
+                    Menu(selectedWeather) {
+                        Button("Sunny ☀️") { selectedWeather = "Sunny" }
+                        Button("Rainy 🌧") { selectedWeather = "Rainy" }
+                        Button("Cloudy ☁️") { selectedWeather = "Cloudy" }
+                        Button("Windy 🌬") { selectedWeather = "Windy" }
+                        Button("Snowy ❄️") { selectedWeather = "Snowy" }
+                        Button("Foggy 🌫") { selectedWeather = "Foggy" }
+                        Button("Stormy ⛈") { selectedWeather = "Stormy" }
+                    }
+                    
+                    // Menu for Outfit Choice
+                    Menu(selectedOutfit) {
+                        Button("Casual Outfit - Everyday Comfort") { selectedOutfit = "Casual Outfit" }
+                        Button("Formal Outfit - Business and Events") { selectedOutfit = "Formal Outfit" }
+                        Button("Fancy Outfit - Elegant & Luxurious") { selectedOutfit = "Fancy Outfit" }
+                        Button("Vintage Outfit - Timeless Classics") { selectedOutfit = "Vintage Outfit" }
+                        Button("Streetwear Outfit - Urban & Trendy") { selectedOutfit = "Streetwear Outfit" }
+                        Button("Preppy Outfit - Polished & Pre-collegiate") { selectedOutfit = "Preppy Outfit" }
+                        Button("Minimalist Outfit - Simple & Sleek") { selectedOutfit = "Minimalist Outfit" }
+                        Button("Comfy Outfit - Cozy & Relaxed") { selectedOutfit = "Comfy Outfit" }
+                        Button("Artsy Outfit - Creative & Unique") { selectedOutfit = "Artsy Outfit" }
+                    }
+                    
+                    // Ideal Time
                     Button("Best Time") { showingTime = true }
-                        .padding()
+                    // .padding()
                         .sheet(isPresented: $showingTime) {
                             DatePicker(
                                 "Select Time", selection: $selectedTime, displayedComponents: .hourAndMinute
@@ -85,23 +97,24 @@ struct SelectedPage: View {
                             .labelsHidden()
                             
                             Text("Selected time: \(timeFormatter.string(from: selectedTime))")
-                                .padding()
                             
                             Button("Done") { showingTime = false }
                         }
+                        .padding(.all)
                     Text("Selected time: \(timeFormatter.string(from: selectedTime))")
                 }
-                .padding(.all)
+                .padding(.top, 25)
+                .font(.system(size: 25))
+                .padding(.bottom, 50)
             }
-            .font(.system(size: 25))
         }
         .onAppear {
             if userId == nil {
-                userId = Auth.auth().currentUser?.uid  
+                userId = Auth.auth().currentUser?.uid
             }
         }
     }
-
+    
     func saveToFirebase(userId: String) {
         let data = [
             "Title": selectedTitle,
@@ -110,7 +123,7 @@ struct SelectedPage: View {
             "Description": selectedDescription,
             "Time": selectedTime
         ] as [String: Any]
-
+        
         db.collection("User").document(userId).collection("Archive").addDocument(data: data) { err in
             if let err = err {
                 print("Error adding document: \(err)")
