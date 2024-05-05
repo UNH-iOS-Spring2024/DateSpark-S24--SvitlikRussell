caf684bec80693ea70cfbab64722c085dc1f29de
sarahsvitlik03 <sarahtheawesome03@gmail.com>
2024-05-05 11:54:48 -0400

    UI Updates to friends page

--- DateSpark/Friends/View/FriendRequestView.swift
+++ DateSpark/Friends/View/FriendRequestView.swift
@@ -5,31 +5,56 @@ import SwiftUI
 struct FriendRequestView: View {
     @ObservedObject var viewModel: FriendsViewModel
     var request: FriendRequest
+    let titleFont = Font.largeTitle.lowercaseSmallCaps()
+    @State private var accepted = false
+    @State private var rejected = false
 
     var body: some View {
-        HStack {
-            Text("\(request.from) added you as a friend")
-                .font(.headline)
-
-            HStack {
-                Button("Accept") {
-                    viewModel.respondToRequest(request, accept: true)
+        VStack  {
+                    
+                    VStack{
+                        Image("Logo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300, height: 300)
+                    }
+                    
+                    Text("Requests")
+                        .lineLimit(2)
+                        .font(titleFont)
+                        .bold()
+                        .foregroundColor(CustomColors.beige)
+                    
+                    HStack {
+                        Text("\(request.from)")
+                            .font(.headline)
+                            
+                        Button(action: {
+                            viewModel.respondToRequest(request, accept: true)
+                            accepted = true
+                        }) {
+                            Text(accepted ? "Accepted" : "Accept")
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(accepted ? Color.gray : CustomColors.lightPink)
+                        .disabled(request.status != .pending || accepted || rejected)
+                        
+                        Button(action: {
+                            viewModel.respondToRequest(request, accept: false)
+                            rejected = true
+                        }) {
+                            Text(rejected ? "Rejected" : "Reject")
+                        }
+                        .buttonStyle(.bordered)
+                        .tint(rejected ? Color.gray : CustomColors.darkRed)
+                        .disabled(request.status != .pending || accepted || rejected)
+                        
+                    }
+                    .padding()
                 }
-                .buttonStyle(.borderedProminent)
-                .disabled(request.status != .pending)
-                
-                Button("Reject") {
-                    viewModel.respondToRequest(request, accept: false)
-                }
-                .buttonStyle(.bordered)
-                .disabled(request.status != .pending)
-
             }
         }
-        .padding()
-        .frame(width: 300, height: 100)
-    }
-}
+
 
 struct FriendRequestView_Previews: PreviewProvider {
     static var previews: some View {
